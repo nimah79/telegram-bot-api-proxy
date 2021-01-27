b6743ce479c2d7416ad40c64d725f71be1a6cc77
Nima HeydariNasab <nimah79@users.noreply.github.com>
2021-01-27 14:35:01 +0000

    Apply fixes from StyleCI

--- index.php
+++ index.php
@@ -5,29 +5,29 @@
  * By NimaH79
  * NimaH79.ir.
  */
-$scheme = !empty($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on" || !empty($headers["X-Forwarded-Proto"]) && $headers["X-Forwarded-Proto"] == "https" ? "https" : "http";
-$uri = parse_url((preg_match("/^\w+:\/\//", $_SERVER["REQUEST_URI"]) ? "" : $scheme."://".$_SERVER["SERVER_NAME"]).$_SERVER["REQUEST_URI"]);
-$_SERVER["REQUEST_URI"] = $uri["path"].(isset($uri["query"]) ? "?".$uri["query"] : "").(isset($uri["fragment"]) ? "#".$uri["fragment"] : "");
-$base = rtrim(str_replace("\\", "/", dirname($_SERVER["SCRIPT_NAME"])));
-$uri = parse_url((preg_match("/^\w+:\/\//", $_SERVER["REQUEST_URI"]) ? "" : $scheme."://".$_SERVER["SERVER_NAME"]).$_SERVER["REQUEST_URI"]);
-$query = preg_replace("/^".preg_quote($base, "/")."/", "", $uri["path"]);
+$scheme = !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' || !empty($headers['X-Forwarded-Proto']) && $headers['X-Forwarded-Proto'] == 'https' ? 'https' : 'http';
+$uri = parse_url((preg_match("/^\w+:\/\//", $_SERVER['REQUEST_URI']) ? '' : $scheme.'://'.$_SERVER['SERVER_NAME']).$_SERVER['REQUEST_URI']);
+$_SERVER['REQUEST_URI'] = $uri['path'].(isset($uri['query']) ? '?'.$uri['query'] : '').(isset($uri['fragment']) ? '#'.$uri['fragment'] : '');
+$base = rtrim(str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME'])));
+$uri = parse_url((preg_match("/^\w+:\/\//", $_SERVER['REQUEST_URI']) ? '' : $scheme.'://'.$_SERVER['SERVER_NAME']).$_SERVER['REQUEST_URI']);
+$query = preg_replace('/^'.preg_quote($base, '/').'/', '', $uri['path']);
 
-header("Content-Type: application/json");
+header('Content-Type: application/json');
 
-if (empty($query) || $query == "/") {
+if (empty($query) || $query == '/') {
     $response_code = 400;
     http_response_code($response_code);
-    echo json_encode(["ok" => false, "error_code" => $response_code, "description" => "request query should not be empty"]);
-    die;
+    echo json_encode(['ok' => false, 'error_code' => $response_code, 'description' => 'request query should not be empty']);
+    exit;
 }
-$ch = curl_init("https://api.telegram.org".(substr($query, 0, 4) == "/" ? "" : "/").$query);
+$ch = curl_init('https://api.telegram.org'.(substr($query, 0, 4) == '/' ? '' : '/').$query);
 curl_setopt_array($ch, [
-    CURLOPT_HEADER => true,
+    CURLOPT_HEADER         => true,
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_FOLLOWLOCATION => true,
     CURLOPT_SSL_VERIFYPEER => false,
     CURLOPT_SSL_VERIFYHOST => false,
-    CURLOPT_POSTFIELDS => file_get_contents("php://input"),
+    CURLOPT_POSTFIELDS     => file_get_contents('php://input'),
 ]);
 $response = curl_exec($ch);
 curl_close($ch);
